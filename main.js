1f7e4be2ae7a4a7d6a6923651e21c73c6d4b084a
kimballtyler <kimballstyler@gmail.com>
2020-08-15 17:06:40 -0400

    css and size responsiveness and design

--- main.js
+++ main.js
@@ -12,9 +12,10 @@ var modalButton = document.getElementById("modal-button");
 var modalHeader = document.getElementById("modal-header");
 var homeIcon = document.getElementById("home-icon");
 var homeIcon2 = document.getElementById("home-icon2");
+var backButton = document.getElementById("back-button");
 var searchValue;
 var searchValueNew;
-var maxLoops = 6;
+var maxLoops;
 
 var comicText;
 var dateText;
@@ -53,9 +54,9 @@ function handleEventsErrorCheck(events) {
     searchButton.classList.add("hidden");
     loadingButton.classList.remove("hidden");
     dataObj = events;
-    eventsWithoutAttractions(dataObj);
+    eventsWithoutAttractions(events);
     setTimeout(function() {
-      handleEventsSuccess(dataObj);
+      handleEventsSuccess(events);
     }, 1000);
   }
 }
@@ -75,12 +76,11 @@ function searchValueSpaces(searchValue) {
 }
 
 function maxLoopsDefine() {
-  if (dataObj._embedded.events.length < 6) {
-    maxLoops = dataObj._embedded.events.length;
-  }
+  maxLoops = dataObj._embedded.events.length;
 }
 
 function eventsWithoutAttractions(dataObj) {
+  maxLoopsDefine();
   for (var i = 0; i < maxLoops; i++) {
     if (!dataObj._embedded.events[i]._embedded.attractions) {
       dataObj._embedded.events.splice(i, 1);
@@ -106,12 +106,13 @@ function handleEventsSuccess(dataObj) {
 }
 
 function displayEvents() {
+  document.getElementById("events-row-container").innerHTML = "";
   for (var i = 0; i < maxLoops; i++) {
     var row = document.createElement("div");
-    row.setAttribute("class", "row pt-4 ml-1 text-center");
-    eventsPage.appendChild(row);
+    row.setAttribute("class", "row pt-3 pb-2 text-center d-flex justify-content-center");
+    document.getElementById("events-row-container").appendChild(row);
     var col1 = document.createElement("div");
-    col1.setAttribute("class", "col");
+    col1.setAttribute("class", "col-sm-3  lg-2 xl-1 col-6 pl-0");
     row.appendChild(col1);
     var p1 = document.createElement("p");
     var addI = i + 1;
@@ -121,17 +122,21 @@ function displayEvents() {
     var p2 = document.createElement("p");
     var dateId = "date-text" + addI;
     p2.setAttribute("id", dateId);
+    p1.style.fontSize = "1.2rem";
+    p2.style.fontSize = "1.2rem";
     col1.appendChild(p2);
     var col2 = document.createElement("div");
-    col2.setAttribute("class", "col pt-2");
+    col2.setAttribute("class", "col-6 col-sm-3 lg-2 xl-1 pl-0 pt-1");
     row.appendChild(col2);
     var btn = document.createElement("button");
     var btnId = "choose-event" + addI;
     btn.setAttribute("id", btnId);
     btn.setAttribute("type", "button");
-    btn.setAttribute("class", "btn btn-primary");
+    btn.setAttribute("class", "btn btn-outline-dark");
     btn.setAttribute("index", i);
-    btn.textContent = "Choose Event";
+    btn.textContent = "View Event";
+    btn.style.color = "black";
+    btn.style.boxShadow = "4px 9px 10px maroon";
     btn.addEventListener("click", function(event) {
       var btnIndex = event.target.getAttribute("index");
       finalPageEvent(btnIndex, dataObj);
@@ -151,6 +156,11 @@ homeIcon.addEventListener("click", function() {
 
 homeIcon2.addEventListener("click", function () {
   searchPage.classList.remove("hidden");
+  finalPage.classList.add("hidden");
+});
+
+backButton.addEventListener("click", function() {
+  eventsPage.classList.remove("hidden");
   finalPage.classList.add("hidden");
 });
 
